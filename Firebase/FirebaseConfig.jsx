c513dcafa096311b0f572fcfbd36982ea0c05a21
Thando <thandozigxashi34@gmail.com>
2025-11-13 13:11:05 +0200

    Moved my API key to a .env

--- Firebase/FirebaseConfig.jsx
+++ Firebase/FirebaseConfig.jsx
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnYXF7RGdihIvGYG8yg2-Sa6_sndCSk58",
-  authDomain: "flow-c6163.firebaseapp.com",
-  projectId: "flow-c6163",
-  storageBucket: "flow-c6163.firebasestorage.app",
-  messagingSenderId: "117376198974",
-  appId: "1:117376198974:web:387ffba9f5763148d2a46f"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
